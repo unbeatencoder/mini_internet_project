343d72aae04f35d63c231ef6ba63a782a75b3b2c
trevor <tkann@andrew.cmu.edu>
2022-04-23 18:03:53 -0400

    added new Dockerfiles for the bitcoin hosts.

---
+++ btcd_Dockerfiles/startup.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+####start up necesarry daemons####
+
+#BTC
+bitcoind -daemon
+
+#SSH
+sed -i 's/#PasswordAuthentication yes/PasswordAuthentication no/g' /etc/ssh/sshd_config
+mkdir -p /var/run/sshd
+chmod 0755 /var/run/sshd
+/usr/sbin/sshd
+
+### ensure they start properly ###
+sleep 4
+
+# import env name
+NAME=$RANDOM
+export NAME=$NAME
+
+# make wallet and readback info
+echo $NAME
+bitcoin-cli createwallet $NAME
+bitcoin-cli -getinfo
+
+#launch a shell so it keeps alive forever
+/bin/bash
